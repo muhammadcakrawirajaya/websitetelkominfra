3b1b92bc0f24ba1e9fce1d71e308e217107c839d
Cakra Wirajaya <cakrawirajaya@Cakras-MacBook-Air.local>
2024-10-31 16:06:54 +0700

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        Copyright &copy; <strong><span>Telkom Infrastruktur Sidoarjo</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Developed by <a href="https://www.instagram.com/cakrawirajaya/" target="blank">Cakra Wirajaya</a> <span>and</span> <a href="#">Ega Syahrul</a>
+    </div>
+</footer>
